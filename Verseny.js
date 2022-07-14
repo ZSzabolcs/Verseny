2062b474f1d7f7d73dabcfd3da0b4d46920e1486
ZSzabolcs <90418566+ZSzabolcs@users.noreply.github.com>
2022-07-14 19:05:48 +0200

    Add files via upload

--- Verseny.js
+++ Verseny.js
@@ -47,29 +47,52 @@ function start()
 {
     document.getElementById("button").style.display="";
     document.getElementById("tree").style.display="none";
+    document.getElementById("button2").style.display="";
+    document.getElementById("button3").style.display="";
 }
 
 function karacsonyfaizalodas()
 {
     document.getElementById("button").style.display="none";
     document.getElementById("tree").style.display="";
-    document.getElementById("button2").style.display="";
 }
 
 function matek()
 {
     var szamok = new Array();
 
-    alert("Bekérek öntől 5 számot.")
+    alert("Bekérek öntől 5db számot.")
 
     szamok[0] = parseInt(prompt("Kérem írjon be egy számot!"));
     szamok[1] = parseInt(prompt("Kérem írjon be még egy számot!"));
     szamok[2] = parseInt(prompt("Kérem írjon be még egy számot!"));
     szamok[3] = parseInt(prompt("Kérem írjon be még egy számot!"));
-    szamok[4] = parseInt(prompt("Kérem írjon be még egy számot!"));
+    szamok[4] = parseInt(prompt("Kérem írjon be még egy számot!")); 
 
     document.getElementById("button2").style.display="none";
 
     document.getElementById("numbers").innerHTML="A számok a következők: " + szamok[0] + "; " + szamok[1] + "; " + szamok[2] + "; " + szamok[3] + "; " + szamok[4] + ";";
     document.getElementById("numbers2").innerHTML="A számok négyzetei a következők: " + szamok[0] * szamok[0]+ "; " + szamok[1] * szamok[1] + "; " + szamok[2] * szamok[2] + "; " + szamok[3] * szamok[3] + "; " + szamok[4] * szamok[4] + ";";
+}
+
+function matek2()
+{
+    var x = parseInt(prompt("Kérem írjon be egy tizedestört alakú számot! (A tizedesvessző hejére tegyen egy pontot!"));
+
+    xup = Math.ceil(x);
+    xdown = Math.floor(x);
+
+    if(xup <= x+0.5)
+    {
+        xrounded = xup;
+    }
+
+    else if(x+0.5 < xup)
+    {
+        xrounded = xdown;
+    }
+
+    document.getElementById("button3").style.display="none";
+
+    document.getElementById("numbers3").innerHTML="A beírt szám egészre kerekített értéke: " + xrounded + ";";
 }
